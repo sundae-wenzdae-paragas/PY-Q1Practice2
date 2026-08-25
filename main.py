a5750f249d495a2d6f733aac44ce8eb9e0c7f68a
If a Sundae exists then a Wenzdae also exists! <paragas.wp@obmontessori.edu.ph>
2026-08-25 08:45:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ y = 2025 #integer
 z = 3.14 #float
 a = True #bool
 b = ['Ako', 'Ay', 'May', 'Lobo'] #list
+c = (1,2,3) #tuple
 
 display(type(x), target="div1") #display output in div
 display(type(y), target="div1") 
 display(type(z), target="div1")
 display(type(a), target="div1")
 display(type(b), target="div1")
+display(type(c), target="div1")
