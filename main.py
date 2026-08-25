754f4f346855f20958653d22977dfc69421c2c32
If a Sundae exists then a Wenzdae also exists! <paragas.wp@obmontessori.edu.ph>
2026-08-25 08:40:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from pyscript import display
 x = "Year" #string
 y = 2025 #integer
 z = 3.14 #float
+a = True #bool
 
 display(type(x), target="div1") #display output in div
 display(type(y), target="div1") 
 display(type(z), target="div1")
-
+display(type(a), target="div1")
