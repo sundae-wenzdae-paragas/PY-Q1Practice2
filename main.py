78e1d6b40bdcbf547b5dd382d620cbdc68641856
If a Sundae exists then a Wenzdae also exists! <paragas.wp@obmontessori.edu.ph>
2026-08-25 08:51:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ z = 3.14 #float
 a = True #bool
 b = ['Ako', 'Ay', 'May', 'Lobo'] #list
 c = (1,2,3) #tuple
+d = {1,2,3} #set with int
+e = {'emerald', 'ruby', 'sapphire'} #set with string
+f = 
 
-display(type(x), target="div1") #display output in div
+display('The data type of x is ', type(x), target="div1") #display output in div
 display(type(y), target="div1") 
 display(type(z), target="div1")
 display(type(a), target="div1")
 display(type(b), target="div1")
 display(type(c), target="div1")
+display(type(d), target="div1")
+display(type(e), target="div1")
+display(type(f), target="div1")
